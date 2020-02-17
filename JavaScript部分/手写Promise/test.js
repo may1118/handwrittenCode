2aa85de04a504337dcd987b776b89661f2ec4584
may1118 <517537976@qq.com>
2020-02-17 18:54:33 +0800

    关于promise还是有疑问

--- JavaScript部分/手写Promise/test.js
+++ JavaScript部分/手写Promise/test.js
@@ -1,47 +1,5 @@
-const myPromise = require('./index');
-let promise = new myPromise((resolve, reject) => {
-  setTimeout(_ => {
-    resolve(100);
-  }, 1000);
+const myPromise = require('./promise');
+let promise = new myPromise((resolve, reject) => {});
+promise.catch(_ => {
+  console.log('error');
 });
-promise
-  .then(
-    res => {
-      return 100;
-      new myPromise((res, rej) => {
-        res(
-          new myPromise((res, rej) => {
-            res('===');
-          })
-        );
-      });
-    },
-    err => {
-      return err;
-    }
-  )
-  .then(
-    res => {
-      console.log('res: ', res);
-      return res;
-    },
-    err => {
-      console.log('err: ', err);
-      return err;
-    }
-  );
-// promise.catch(_ => {
-//   console.log('error');
-// });
-// promise
-//   .all(
-//     new myPromise(res => {
-//       res(100);
-//     }),
-//     new myPromise(res => {
-//       res(100);
-//     })
-//   )
-//   .then(res => {
-//     console.log(res);
-//   });
